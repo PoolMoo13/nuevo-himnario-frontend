e3ba1f9ec7631fff00b8dea9360215069c9faf03
Daniel <cesar4herrera@gmail.com>
2024-08-27 12:07:32 -0600

    Update favicon and page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Container, Anchor, Group, Burger, Box, Image, Button } from "@mantine/core";
 import { theme } from "./theme";
+import { useDisclosure } from '@mantine/hooks';
+// import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './DoubleHeader.module.css';
 
 interface Props {
   children?: ReactNode
 }
 
-export default function App ({ children }: Props) {
-  return <MantineProvider theme={theme}> 
-    <h1>Header</h1>
+export default function App({ children }: Props) {
+
+  return <MantineProvider theme={theme}>
+
+    <header className={classes.header}>
+      <Container className={classes.inner}>
+        <Image
+          src="https://raw.githubusercontent.com/PoolMoo13/himnario-jovenes/master/public/logo192.png"
+          height={40}
+        />
+        <h3> Himno 1 </h3>
+      </Container>
+    </header>
+    <Image
+      src="https://raw.githubusercontent.com/PoolMoo13/himnario-jovenes/master/public/logo192.png"
+      h={90}
+      w={409}
+      fit="contain"
+    />
     {
       children
     }
+
+    <Button 
+    variant="light"
+    w={400}
+    >
+      Entrar
+    </Button>
 
   </MantineProvider>;
 }
