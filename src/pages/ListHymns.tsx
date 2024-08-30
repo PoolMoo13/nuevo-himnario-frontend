2f47a3e3b1f216829afe3527ad1d1e8060e470f4
Daniel <cesar4herrera@gmail.com>
2024-08-30 09:28:46 -0600

    buscar en lista de himnos

--- src/pages/ListHymns.tsx
+++ src/pages/ListHymns.tsx
@@ -1,4 +1,5 @@
 import { Autocomplete } from "@mantine/core";
+import { useState, useEffect } from "react";
 
 interface Hymn {
     id: number;
@@ -7,32 +8,48 @@ interface Hymn {
 }
 
 const ListHymns = () => {
+    const [hymnData, setHymnData] = useState<{ value: string, label: string }[]>([]);
+    const [allTitles, setAllTitles] = useState<string[]>([]);
 
     const getHimnarios = async () => {
-
         const url = `http://localhost:3001/api/hymnals/`;
         const res = await fetch(url);
         const { data } = await res.json();
 
-        const himnos = data.map((hymn: Hymn) =>  ({
-            id: hymn._id,
-            description: hymn.description,
-            ids: hymn.hymnns.map((h: { id: string }) => String(h.id)).join(', ')
-        }));
+        const himnos = data.map((hymn: Hymn) => hymn.hymnns.map((h: { id: number, title: string, lyrics: string }) => ({
+            value: h.title,
+            label: `${h.title} - ${h.lyrics}`
+        }))).flat();
+
+        const titles = himnos.map(h => h.value);
+
+        setHymnData(himnos);
+        setAllTitles(titles);
         console.log("🚀 ~ file: ListHymns.tsx:16 ~ himnos ~ himnos:", himnos);
         console.log("🚀 ~ file: Home.tsx:11 ~ getHimnarios ~ data:", data);
     };
 
-    getHimnarios();
+    useEffect(() => {
+        getHimnarios();
+    }, []);
 
-    return (<>
-        <h1>Lista de himnos</h1>
+    return (
+        <>
+            <h1>Lista de himnos</h1>
 
-        <Autocomplete
-            placeholder="Buscar Himnos"
-            data={[ 'Himno 1', 'Himno 2', 'Himno 3' ]}
-        />
-    </>);
+            <Autocomplete
+                placeholder="Buscar Himnos"
+                data={hymnData}
+            />
+
+            <h2>Todos los títulos de los himnos</h2>
+            <ul>
+                {allTitles.map((title, index) => (
+                    <li key={index}>{title}</li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export default ListHymns;
